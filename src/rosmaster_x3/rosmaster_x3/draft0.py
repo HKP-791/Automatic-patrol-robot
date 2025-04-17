8a0516f6dffef7eaee19fce5370edb7502759a25
Calcium_Iodide <desprado233@163.com>
2025-04-17 15:01:34 +0800

    修改了readme文件

--- src/rosmaster_x3/rosmaster_x3/draft0.py
+++ src/rosmaster_x3/rosmaster_x3/draft0.py
@@ -1,7 +1,44 @@
-class calculator:
-    def __init__(self, num):
-        self.num = num
-    
-    def cal(self):
-        self.num += 1
-        return self.num
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+import numpy as np
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.bridge = CvBridge()
+        self.latest_image = None
+        self.queue_ = []
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera_sensor/image_raw',
+            self.image_callback,
+            10)
+        self.publisher_ = self.create_publisher(
+            Image,
+            '/camera_sensor/processed_image',
+            10)
+
+    def image_callback(self, msg):
+        self.latest_image = msg
+        if self.latest_image is not None:
+            cv_image = self.bridge.imgmsg_to_cv2(self.latest_image)
+        self.cv_image = cv_image
+
+    def image_pub(self):
+        while self.queue_:
+            msg = Image()
+            msg.data = self.queue_.pop(0)
+            self.publisher_.publish(msg)
+            self.get_logger().info('Publishing image')
+
+def main(args=None):
+    rclpy.init(args=args)
+    image_subscriber = ImageSubscriber()
+    rclpy.spin(image_subscriber)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
